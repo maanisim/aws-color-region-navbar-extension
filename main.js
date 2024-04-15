4128b0b82fde10b15796db33810a59333c87ea04
maanisim <michal.anisimow@gmail.com>
2024-04-15 22:17:05 +0100

    Added more region, fixed for new AWS navbar and added MutationObserver

--- main.js
+++ main.js
@@ -1,51 +1,71 @@
 const defaultColor = {
-    'N. Virginia': '#3cb44b',
-    Ohio: '#e6194b',
+    'N. Virginia': '#e6194b',
+    Ohio: '#3cb44b',
     'N. California': '#e58e00',
     Oregon: '#4363d8',
     'Cape Town': '#16ab3e',
-    'Hong Kong': '#008080',
-    Mumbai: '#8a7299',
-    Seoul: '#9a6324',
-    Singapore: '#ccc8a0',
-    Sydney: '#800000',
-    Tokyo: '#669975',
-    Central: '#f58231',
-    Frankfurt: '#911eb4',
-    Ireland: '#31a8a8',
-    London: '#f032e6',
-    Milan: '#a30f88',
-    Paris: '#83ac08',
-    Stockholm: '#c89898',
-    Bahrain: '#808000',
-    'São Paulo': '#b2977b'
+    'Hong Kong': '#FFD700',
+    Hyderabad: '#f7c8e0',
+    Jakarta: '#FF4500',
+    Melbourne: '#8A2BE2',
+    Mumbai: '#9932CC',
+    Osaka: '#00CED1',
+    Seoul: '#FF1493',
+    Singapore: '#FF8C00',
+    Sydney: '#7CFC00',
+    Tokyo: '#4682B4',
+    Central: '#D2691E',
+    Calgary: '#20B2AA',
+    Frankfurt: '#9370DB',
+    Ireland: '#2E8B57',
+    London: '#4B0082',
+    Milan: '#8B4513',
+    Paris: '#CD5C5C',
+    Spain: '#008B8B',
+    Stockholm: '#d2cb75',
+    Zurich: '#800000',
+    Bahrain: '#6B8E23',
+    UAE: '#008080',
+    'Tel Aviv': '#000080',
+    'São Paulo': '#B22222',
+    'US-Gov-East': '#556B2F',
+    'US-Gov-West': '#483D8B'
+};
+
+
+// Function to apply color to navbar
+function applyNavbarColor() {
+    const regionButton = document.querySelector('[data-testid="awsc-nav-regions-menu-button"] > span');
+    if (regionButton) {
+        const region = regionButton.innerText.trim();
+        console.log(region);
+
+        browser.storage.local.get(region, function(results) {
+            let color = results[region];
+            if (color === undefined) {
+                color = defaultColor[region];
+            }
+            if (color) {
+                document.querySelector("#awsc-top-level-nav").style.backgroundColor = color;
+            }
+        });
+    }
 }
 
+// Function to observe changes in the DOM and trigger applyNavbarColor when regionButton appears
+function observeDOM() {
+    const observer = new MutationObserver(function(mutationsList) {
+        mutationsList.forEach(function(mutation) {
+            if (mutation.type === 'childList') {
+                if (!document.querySelector("#awsc-top-level-nav").style.backgroundColor) {
+                    applyNavbarColor();
+                    observer.disconnect(); // Disconnect the observer after applying color once, otherwise we see a spike of calls at the page load
+                }
+            }
+        });
+    });
+    observer.observe(document.body, { childList: true, subtree: true });
+}
 
-
-setTimeout(() => {
-const region = document.querySelector('[data-testid="awsc-nav-regions-menu-button"]>span').innerText;
-console.log(region);
-
-chrome.storage.local.get(region, (results)=>{
-    let color = results[region];
-    if(color===undefined){
-        color = defaultColor[region]
-    }
-    if(color===undefined){
-        return;
-    }
-    document.querySelector("#awsc-top-level-nav").style.backgroundColor=color;
-
-// Feel free to uncomment and modify the code below to change the color of other elements based on the acct number... 
-// It's just a starter, so you'll have to modify it to work for your use case.. DIY other methods if you want.
-
-// const accountNumber = document.getElementById("menu--account").children[0].children[0].children[0].children[1].innerText.replace(/-/g, "");
-// if (accountNumber=="123456789012"){
-//     document.querySelector('[aria-controls="menu--account"]').style.backgroundColor="#FFA7C3";
-// }
-
-
-})
-
-}, 3000);
+// Start observing the DOM
+observeDOM();
